085c76f21040e56643a645392280e27f9a85d58b
Arhan-55 <75885658+Arhan-55@users.noreply.github.com>
2024-02-09 15:38:10 +0530

    Update Sorting.cpp

--- Sorting.cpp
+++ Sorting.cpp
@@ -1,16 +1,43 @@
+// sum , avg , sorting in assending
 #include <iostream>
 using namespace std;
 
-int b = 10;
+int main()
+{
+    int a[10];
+    int sum = 0, avg = 1;
 
+    cout << "Enter the arr: ";
 
-int main(){
+    for (int i = 0; i < 10; i++)
+    {
+        cin >> a[i];
+        sum += a[i];
+    }
 
-    int b = 20;
+    cout << "Sum of arr = " << sum << endl;
+    avg = sum / 10;
+    cout << "Avg of arr = " << avg << endl;
 
-    cout<<"The Local value: "<< b;
-    cout<<"\nGlobal value: "<< ::b << endl;
+    int temp;
 
-    return 1;
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = i; j < 10; j++)
+        {
+            if (a[j] < a[i])
+            {
+                temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
 
+    for (int i = 0; i < 10; i++)
+    {
+        cout << a[i] << " ";
+    }
+
+    return 0;
 }
